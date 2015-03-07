c8b76937ecc48d0a799ee8f45b34b808c042c89f
Alan Carden <acarden120@gmail.com>
2015-03-07 14:45:15 +0000

    First commit for new_options rails app

---
+++ README.md
@@ -0,0 +1,6 @@
+== README
+## new_options: a rails application created as part of the submission for checkpoint 20 - Basic Rails App
+
+## Options: 
+# -q, [--quiet], [--no-quiet]      # Suppress status output
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewOptions::Application.config.secret_key_base = '35b6c3002169a2ac81a6519abd4880485a53b73871503264d3b7d526ff18f72b97ab23618ad4ed563c6750f700a3021a340fc4e956722918a0fe9e9a18340c4c'
